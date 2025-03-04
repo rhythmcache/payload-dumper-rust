2eb0ae80c74ee9df5fcaf8bfbe92bbb9784ae129
rhythmcache <153998419+rhythmcache@users.noreply.github.com>
2025-03-04 06:44:53 -0800

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,4 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+      let mut config = prost_build::Config::new();
+    config.type_attribute(".", "#[derive(serde::Serialize)]");
     prost_build::compile_protos(&["proto/update_metadata.proto"], &["proto/"])?;
     Ok(())
 }
