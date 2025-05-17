2444b01c01ef0d295a079fbba58b32edc1a03f31
rhythmcache <153998419+rhythmcache@users.noreply.github.com>
2025-05-17 22:59:12 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::io::{Read, Seek};
 
 trait ReadSeek: Read + Seek {}
 impl<T: Read + Seek> ReadSeek for T {}
-include!(concat!(env!("OUT_DIR"), "/chromeos_update_engine.rs"));
+include!("proto/update_metadata.rs");
 const BSDF2_MAGIC: &[u8] = b"BSDF2";
 
 fn main() {
