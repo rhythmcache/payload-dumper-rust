f972873787bdd747097b7964d6a0746b8046d26e
rhythmcache <153998419+rhythmcache@users.noreply.github.com>
2025-05-18 08:36:48 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use std::io::{Read, Seek};
 trait ReadSeek: Read + Seek {}
 impl<T: Read + Seek> ReadSeek for T {}
 include!("proto/update_metadata.rs");
-const BSDF2_MAGIC: &[u8] = b"BSDF2";
 
 fn main() {
     let _ = module::entry::run();
