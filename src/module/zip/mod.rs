4d3c2a0adac7dcb692d657a1f50eec9b90e84c4c
rhythmcache <153998419+rhythmcache@users.noreply.github.com>
2025-10-31 23:57:02 +0530

    Change cfg condition for zip_core module

--- src/module/zip/mod.rs
+++ src/module/zip/mod.rs
@@ -4,5 +4,5 @@ pub mod local_zip;
 #[cfg(feature = "remote_ota")]
 pub mod remote_zip;
 
-#[cfg(all(feature = "remote_ota", feature = "local_zip"))]
+#[cfg(any(feature = "remote_ota", feature = "local_zip"))]
 pub mod zip_core;
