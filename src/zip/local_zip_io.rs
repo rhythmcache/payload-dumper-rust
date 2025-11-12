fb2293051db46d81a1e59002a02a2d87d3c32999
⚘ कलुआ बिल्ला 😾 <219769887+KaluaBilla@users.noreply.github.com>
2025-11-12 17:50:09 +0530

    pread

--- src/zip/local_zip_io.rs
+++ src/zip/local_zip_io.rs
@@ -2,35 +2,37 @@ use crate::zip::zip_io::ZipIO;
 use anyhow::Result;
 use async_trait::async_trait;
 use std::path::PathBuf;
-use std::sync::Arc;
-use tokio::fs::File;
-use tokio::io::{AsyncReadExt, AsyncSeekExt};
-use tokio::sync::Mutex;
 
 pub struct LocalZipIO {
-    file: Arc<Mutex<File>>,
+    file: std::fs::File, 
     size: u64,
 }
 
 impl LocalZipIO {
     pub async fn new(path: PathBuf) -> Result<Self> {
-        let file = File::open(&path).await?;
-        let size = file.metadata().await?.len();
+        let file = std::fs::File::open(&path)?;
+        let size = file.metadata()?.len();
 
-        Ok(Self {
-            file: Arc::new(Mutex::new(file)),
-            size,
-        })
+        Ok(Self { file, size })
     }
 }
 
 #[async_trait]
 impl ZipIO for LocalZipIO {
     async fn read_at(&self, offset: u64, buf: &mut [u8]) -> Result<()> {
-        let mut file = self.file.lock().await;
-        file.seek(std::io::SeekFrom::Start(offset)).await?;
-        file.read_exact(buf).await?;
-        Ok(())
+        tokio::task::block_in_place(|| {
+            #[cfg(unix)]
+            {
+                use std::os::unix::fs::FileExt;
+                self.file.read_exact_at(buf, offset)?;
+            }
+            #[cfg(windows)]
+            {
+                use std::os::windows::fs::FileExt;
+                self.file.seek_read(buf, offset)?;
+            }
+            Ok(())
+        })
     }
 
     async fn size(&self) -> Result<u64> {
