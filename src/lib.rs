7b5a40252f5f2a68ef0d88e35a3620189f0416c0
rhythmcache <153998419+rhythmcache@users.noreply.github.com>
2025-12-11 12:17:08 +0530

    Added jni bindings

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,14 @@
 // Copyright (c) 2025 rhythmcache
 // https://github.com/rhythmcache/payload-dumper-rust
 
+#[cfg(feature = "capi")]
+pub mod capi;
 pub mod constants;
+pub mod extractor;
 #[cfg(feature = "remote_zip")]
 pub mod http;
+#[cfg(feature = "jni")]
+pub mod jni;
 #[cfg(feature = "metadata")]
 pub mod metadata;
 pub mod payload;
@@ -15,7 +20,3 @@ pub mod structs;
 pub mod utils;
 #[cfg(any(feature = "local_zip", feature = "remote_zip"))]
 pub mod zip;
-pub mod extractor;
-#[cfg(feature = "capi")]
-pub mod capi;
-
