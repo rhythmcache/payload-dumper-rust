83be7e9a7530d501981ba179ba9180e38744aed2
⚘ कलुआ बिल्ला 😾 <219769887+KaluaBilla@users.noreply.github.com>
2026-01-10 20:57:39 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use cli::entry::run;
 
 #[tokio::main]
 async fn main() {
-    if let Err(e) = run() {
+    if let Err(e) = run().await {
         eprintln!("Error: {}", e);
         std::process::exit(1);
     }
